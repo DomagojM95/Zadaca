60b57ab209e1e4e4f397db8f609455f80c8995de
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-05-19 18:11:18 +0200

    -

--- fejs.sql
+++ fejs.sql
@@ -11,7 +11,7 @@ create table osoba(
 	prezime varchar(50)not null,
 	datumrodenja datetime,
 	email varchar(50),
-	lozinka int,
+	lozinka varchar(50),
 	brojtel int not null,
 	slika varchar(100),
 	administrator bit not null,
@@ -62,3 +62,39 @@ alter table komentar  add foreign key (osoba) references osoba(sifra);
 alter table objava add foreign key (osoba) references osoba(sifra);
 alter table svidamise add foreign key (objava) references objava(sifra);
 alter table svidamise add foreign key (osoba) references osoba(sifra);
+
+
+insert into osoba(ime,prezime,datumrodenja,email,lozinka,brojtel,slika,administrator,stanje,aktivan,uniqueid)
+values('per','peric','2023-05-07','per@gmai.com','dadasdads',80808080,'slika',0,0,0,'yes'),
+('per','peric','2023-05-07','per@gmai.com','dadasdads',80808080,'slika',0,0,0,'yes'),
+('per','peric','2023-05-07','per@gmai.com','dadasdads',80808080,'slika',0,0,0,'yes');
+
+insert into objava (naslov,upis,vrijemeizrade,ipadresa,osoba)
+values ('naslov','hahah','2023-05-05 17:00:00','81281288122',1),
+('naslov','hahah','2023-05-05 17:00:00','81281288122',3),
+('naslov','hahah','2023-05-05 17:00:00','81281288122',2);
+
+
+insert into komentar(vrijemekomentiranja,opis,objava,osoba)
+values ('2023-04-07 08:00:00','hello',1,1),('2023-04-07 08:00:00','hello',3,1),('2023-04-07 08:00:00','hello',2,1);
+
+
+insert into svidamise(vrijemesvidanja,objava,osoba)
+values ('2023-03-05 05:00:00',1,1),('2023-03-05 05:00:00',2,2),('2023-03-05 05:00:00',3,3);
+
+insert into svidamise_komentar(osoba,komentar)
+values(3,1),(2,2),(3,2);
+
+select *  from osoba;
+
+update osoba set ime='marko'
+where sifra=2;
+
+update osoba set lozinka='LOZINKA'
+where sifra=3;
+
+update svidamise set objava=3
+where sifra=2;
+
+
+delete from osoba where sifra=3;
