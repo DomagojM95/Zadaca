c01f9621f1ea12d4ee8797cc4bd22c022ca682ca
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-06-20 12:05:15 +0200

    -

---
+++ CSHARP/zadaca01/Program.cs
@@ -0,0 +1,46 @@
+﻿//Console.WriteLine("unesi dva cijela broja");
+//Console.WriteLine("prvi broj");
+
+//int b1=int.Parse(Console.ReadLine());
+
+//Console.WriteLine("drugi broj");
+
+//int b2=int.Parse(Console.ReadLine());
+
+//if (b1 > b2)
+//{
+//    Console.WriteLine( b1);
+//}
+//else if (b1 < b2)
+//{
+//    Console.WriteLine(b2);
+//}
+//else if(b2 == b1)
+//{
+//    Console.WriteLine("brojevi su jednaki");
+//}
+
+
+
+
+Console.WriteLine("upisi tri cijela broja");
+
+int b1= int.Parse(Console.ReadLine());
+int b2 = int.Parse(Console.ReadLine());
+int b3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(((b1 > b2) && (b1>b3) ? b1 ) + "broj je najveci") ;
+
+
+//if ((b1 > b2) && (b1 > b3))
+//{
+//    Console.WriteLine((b1) + " " + "je najveci");
+//}
+//else if ((b2 > b1) && (b2 > b3))
+//{
+//    Console.WriteLine((b2) + " " + "je najveci");
+//}
+//else if ((b3 > b1) && (b3 > b2))
+//{
+//    Console.WriteLine((b3) + " " + "je najveci");
+//}
